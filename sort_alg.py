8b181061aca92a962fea182dee65a3a6ac5792b5
JonasRu07 <rpprtjns@gmail.com>
2024-07-13 00:57:20 +0200

    added last_sort function

--- sort_alg.py
+++ sort_alg.py
@@ -16,3 +16,21 @@ def insertion(lst: list) -> list:
                     lst = lst[:i] + [lst[i + 1]] + [lst[i]]
                 finished_sorting = False
     return lst
+
+
+def last_sort(lst: list) -> list:
+    """
+    Takes an unordered list and sorts it.
+    It currently only works with only number-lists
+    :param lst: a list out of numbers (int and/or flt)
+    :return: list
+    """
+    a = lst[0]
+    for n in range(0, len(lst)):
+        for i in range(len(lst) - n):
+            if a > lst[i]:
+                a = lst[i]
+        lst.remove(a)
+        lst.append(a)
+        a = lst[0]
+    return lst
