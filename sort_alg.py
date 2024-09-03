4805ef5a55614b7fb7d2d538c57dc022288a7b10
JonasRu07 <rpprtjns@gmail.com>
2024-09-03 12:08:12 +0200

    improved readability on insertionsort
    switched boolean value and name of while statement

--- sort_alg.py
+++ sort_alg.py
@@ -12,16 +12,16 @@ def insertion(lst: list) -> list:
     :param lst: a list out of numbers (int and/or flt)
     :return: list
     """
-    finished_sorting = False
-    while not finished_sorting:
-        finished_sorting = True
+    sorting = True
+    while sorting:
+        sorting = False
         for i in range(0, len(lst) - 1):
             if lst[i] > lst[i+1]:
                 try:
                     lst = lst[:i] + [lst[i+1]] + [lst[i]] + lst[i+2:]
                 except IndexError:
                     lst = lst[:i] + [lst[i + 1]] + [lst[i]]
-                finished_sorting = False
+                sorting = True
     return lst
 
 
